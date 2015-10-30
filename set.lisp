ab7b448ad07f6fcd00e0b1ef60e278c796793807
Joe's Laptop <jmwright@gmail.com>
2015-10-30 01:29:30 -0500

    Fixed set.lisp

--- set.lisp
+++ set.lisp
@@ -55,7 +55,8 @@
 ;; member of a list that represents a set is :set
 (defun setp (l)
   (cond ((not (listp l)) nil)
-        ((equal (first l) :set) t)
+        ((null l) nil)
+        ((equal (common-lisp:first l) :set) t)
         (t nil)))
 
 ;; 17.20 Define set:first and set:rest in your set file to return the element
@@ -67,13 +68,13 @@
 (defun first (s)
   "Return the first element in set S"
   (check-type s set)
-  (car s))
+  (common-lisp:first (common-lisp:rest s)))
 
 (shadow 'common-lisp:rest)
 (defun rest (s)
   "Return the set S without the first element"
   (check-type s set)
-  (cdr s))
+  (common-lisp:rest (common-lisp:rest s)))
 
 ;; 17.21 Define (insert e s) to return a set just like s, but
 ;; with e added as an additional element.  If e is already in s, s should
@@ -81,26 +82,49 @@
 (defun insert (e s)
   "Returns a set just like s, but with e added as an additional element. If e is already in s, s is returned unchanged."
   (check-type s set:set)
-  (cond ((member e s) s)
-        (t (cons e s))))
+  (cond ((member e (rest s)) s)
+        (t (cons :set (cons e (rest s))))))
 
 ;; 17.22 Define empty to be a function that returns true
 ;; if its one argument is a set with no elements, and false otherwise.
 ;; Make empty an external symbol in the set package
 (defun empty (l)
   "Returns true if its one argument is a set with no elements, and false otherwise."
-  (cond ((and (setp l) (null l)) t)
+  (cond ((and (setp l) (null (common-lisp:rest l))) t)
         (t nil)))
 
 ;; 17.23 Redefine set:union to use the new representation of sets.
-;; Define the helper function union-unlabelled-sets as an internal function.
+;; Define the helper function union-unlabeled-sets as an internal function.
 ;; Again evaluate (union '(a b c d) '(b d e f)) while tracing setp.
+(defun union-unlabeled-sets (s1 s2)
+(cond ((null s1) s2)
+      ((member (common-lisp:first s1) s2) (union-unlabeled-sets (common-lisp:rest s1) s2))
+      (t (cons (common-lisp:first s1) (union-unlabeled-sets (common-lisp:rest s1) s2)))))
+
+(defun union (s1 s2)
+  "Returns the union of the sets S1 and S2."
+  (check-type s1 set)
+  (check-type s2 set)
+  (union-unlabeled-sets (common-lisp:rest s1) (common-lisp:rest s2)))
 
 
 ;; 17.24 The intersection of two sets s1 and s2 is the set consisting
 ;; of those elements that are in s1 and also in s2.  Define intersection
 ;; in your set file, with intersection an external symbol in the set package.
 ;; Shadow lisp:intersection.
+(shadow 'common-lisp:intersection)
+
+(defun intersection-unlabeled-sets (s1 s2)
+  (cond ((or (null s1) (null s2)) nil)
+        ((not (member (common-lisp:first s1) s2)) (intersection-unlabeled-sets (common-lisp:rest s1) s2))
+        (t (cons (common-lisp:first s1) (intersection-unlabeled-sets (common-lisp:rest s1) s2)))))
+
+(defun intersection (s1 s2)
+  "Returns the intersection of sets S1 and S2."
+  (check-type s1 set)
+  (check-type s2 set)
+  (makeset (cons :set (intersection-unlabeled-sets (common-lisp:rest s1) (common-lisp:rest s2)))))
+
 
 
 ;; 17.25 The relative complement of two sets s1 and s2 is
